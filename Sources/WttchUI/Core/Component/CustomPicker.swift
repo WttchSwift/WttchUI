1944adfaa7c4b4e66b921754005e1021c1f016a2
wttch（王冲） <wttch@wttch.com>
2023-07-21 15:36:19 +0800

    CustomPicker

--- Sources/WttchUI/Core/Component/CustomPicker.swift
+++ Sources/WttchUI/Core/Component/CustomPicker.swift
@@ -9,11 +9,13 @@ import SwiftUI
 
 #if os(OSX)
 /// 自定义 Picker，自带的 Picker 无法自定义视图（或者自己不会），所以自定义一个。
-public struct CustomPicker<T: Hashable & Identifiable, Content: View, Label: View>: View {
+public struct CustomPicker<T: Hashable & Identifiable, SelectionContent: View, Content: View, Label: View>: View {
     // 要渲染的数据
     let data: [T]
     // 绑定值
     @Binding var selection: T?
+    // 选择项目的视图
+    private var selectionContent: (T?) -> SelectionContent
     // 选择项目的视图
     private var content: (T?) -> Content
     // 标签
@@ -26,11 +28,16 @@ public struct CustomPicker<T: Hashable & Identifiable, Content: View, Label: Vie
     /// - Parameters:
     ///   - data: 数据列表
     ///   - selection: 绑定值
+    ///   - selectionContent: 已选择值的视图的闭包方法
     ///   - content: 项目渲染视图的闭包方法
     ///   - label: 标签
-    public init(_ data: [T], selection: Binding<T?>, @ViewBuilder content: @escaping (T?) -> Content,  @ViewBuilder label: () -> Label) {
+    public init(_ data: [T], selection: Binding<T?>,
+                @ViewBuilder selectionContent: @escaping (T?) -> SelectionContent,
+                @ViewBuilder content: @escaping (T?) -> Content,
+                @ViewBuilder label: () -> Label) {
         self.data = data
         self._selection = selection
+        self.selectionContent = selectionContent
         self.content = content
         self.label = label()
     }
@@ -39,7 +46,7 @@ public struct CustomPicker<T: Hashable & Identifiable, Content: View, Label: Vie
         HStack {
             label
             HStack {
-                content(selection)
+                selectionContent(selection)
                 Image(systemName: "chevron.down")
                     .rotationEffect(.radians(showPopover ? -.pi : 0))
             }
@@ -92,23 +99,28 @@ struct SwiftUIView_Previews: PreviewProvider {
     static var previews: some View {
         VStack {
             GeometryReader { geo in
-                CustomPicker(datas, selection: .constant(Data(id: 1)), content: { i in
-                    if let i = i {
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 16)
-                                .fill(.pink)
-                            
-                            Text("Data: \(i.id)")
-                                .font(.largeTitle)
+                CustomPicker(datas, selection: .constant(Data(id: 1)),
+                    selectionContent: { data in
+                        Text("\(data?.id ?? -1)")
+                    },
+                    content: { i in
+                        if let i = i {
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 16)
+                                    .fill(.pink)
+                                
+                                Text("Data: \(i.id)")
+                                    .font(.largeTitle)
+                            }
+                            .frame(maxHeight: 36)
+                            .frame(minWidth: geo.size.width - 60)
+                        } else {
+                            Text("请选择")
                         }
-                        .frame(maxHeight: 36)
-                        .frame(minWidth: geo.size.width - 60)
-                    } else {
-                        Text("请选择")
+                    }, label: {
+                        Text("选择")
                     }
-                }, label: {
-                    Text("选择")
-                })
+                )
             }
             Spacer()
         }
